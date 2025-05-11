232ff0e53ad76b1ee182d2212bc75ef5258f38cc
Danilo Valderrrama <danilo.valderramaaa@est.iudigital.edu.co>
2025-05-10 21:02:36 -0500

    Primer commit

---
+++ src/main/java/com/rollerspeed/rollerspeed/controller/MenuController.java
@@ -0,0 +1,39 @@
+package com.rollerspeed.rollerspeed.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class MenuController {
+    @GetMapping("mision")
+    public String mision() {
+        return "mision";
+    }
+
+    @GetMapping("vision")
+    public String vision() {
+        return "vision";
+    }
+
+    @GetMapping("valores")
+    public String valores() {
+        return "valores";
+    }
+
+    @GetMapping("servicios")
+    public String servicios() {
+        return "servicios";
+    }
+
+    @GetMapping("eventos")
+    public String eventos() {
+        return "eventos";
+    }
+
+    @GetMapping("")
+    public String home() {
+        return "home";
+    }
+}
